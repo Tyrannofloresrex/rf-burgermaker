b30b94417d17b4574b5f0232117862effc24b134
Rex Flores <Rexgflores@gmail.com>
2021-01-26 23:44:58 -0800

    updating config

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT ||8000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
+
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
